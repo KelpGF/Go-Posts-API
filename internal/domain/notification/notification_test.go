4eb8bec9252447e39d8839481ed0288eb996efda
KelpGF <kelpgf@gmail.com>
2024-07-22 19:50:19 -0300

    test(Notification): ensure GetErrorsMessage returns a correct message

--- internal/domain/notification/notification_test.go
+++ internal/domain/notification/notification_test.go
@@ -1,6 +1,7 @@
 package notification
 
 import (
+	goErrors "errors"
 	"testing"
 
 	"github.com/KelpGF/Go-Posts-API/internal/domain/errors"
@@ -14,16 +15,19 @@ type NotificationTestSuite struct {
 }
 
 func (suite *NotificationTestSuite) SetupTest() {
-	suite.notification = NewNotification()
+	suite.notification = NewNotification("Test")
 }
 
 func (suite *NotificationTestSuite) TestNotification() {
 	suite.False(suite.notification.HasErrors())
 
 	suite.notification.AddError(errors.NewIsRequiredError("Title"))
+	suite.notification.AddError(goErrors.New("Error"))
+
 	suite.True(suite.notification.HasErrors())
-	suite.Len(suite.notification.GetErrors(), 1)
+	suite.Len(suite.notification.GetErrors(), 2)
 	suite.Equal("Title is required", suite.notification.GetErrors()[0].Error())
+	suite.Equal("Test: Title is required, Error", suite.notification.GetErrorsMessage())
 }
 
 func TestSuite(t *testing.T) {
