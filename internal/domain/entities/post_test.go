f6649660bea823496819ecff7070c50b632eef6f
KelpGF <kelpgf@gmail.com>
2024-07-22 19:12:21 -0300

    test(PostEntity): ensure Notification is works correctly

--- internal/domain/entities/post_test.go
+++ internal/domain/entities/post_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/KelpGF/Go-Posts-API/internal/domain/errors"
 	"github.com/stretchr/testify/suite"
 )
 
@@ -14,14 +15,30 @@ type PostEntityTestSuite struct {
 }
 
 func (suite *PostEntityTestSuite) SetupTest() {
-	suite.post = NewPost("Hello World", "This is a test post", "John Doe", time.Now())
+	post, _ := NewPost("Hello World", "This is a test post", "John Doe", time.Now())
+
+	suite.post = post
 }
 
 func (suite *PostEntityTestSuite) TestPostEntity() {
+	suite.False(suite.post.HasErrors())
+
 	suite.Equal("Hello World", suite.post.GetTitle())
 	suite.Equal("This is a test post", suite.post.GetBody())
 	suite.Equal("John Doe", suite.post.GetAuthorName())
 	suite.Equal(time.Now().Format(time.RFC3339), suite.post.GetPublishedAt().Format(time.RFC3339))
+}
+
+func (suite *PostEntityTestSuite) TestPostEntityValidation() {
+	post, _ := NewPost("", "", "", time.Now())
+
+	suite.True(post.HasErrors())
+	suite.Equal([]error{
+		errors.NewIsRequiredError("Title"),
+		errors.NewIsRequiredError("Body"),
+		errors.NewIsRequiredError("AuthorName"),
+	}, post.notification.GetErrors())
+
 }
 
 func TestSuite(t *testing.T) {
