62aaedae71c6bed1648ede77676de3b8a2960d55
KelpGF <kelpgf@gmail.com>
2024-07-22 23:58:21 -0300

    feat: add database connection

---
+++ internal/infrastructure/infra.go
@@ -0,0 +1,63 @@
+package infrastructure
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/KelpGF/Go-Posts-API/configs"
+	"github.com/KelpGF/Go-Posts-API/internal/infrastructure/entities"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var dbConnection *gorm.DB
+
+func StartDatabase(config *configs.ConfigType) *gorm.DB {
+	dsn := makeDSN(config)
+
+	db := openConnection(dsn)
+
+	runMigrations(db)
+
+	dbConnection = db
+
+	return dbConnection
+}
+
+func makeDSN(config *configs.ConfigType) string {
+	return fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		config.DBHost,
+		config.DBUser,
+		config.DBPassword,
+		config.DBName,
+		config.DBPort,
+	)
+}
+
+func openConnection(dsn string) *gorm.DB {
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatal("Failed to connect to database. \n", err)
+		os.Exit(2)
+	}
+
+	db.Logger = logger.Default.LogMode(logger.Info)
+	log.Println("Database connected")
+
+	return db
+}
+
+func runMigrations(db *gorm.DB) {
+	err := db.AutoMigrate(&entities.Post{})
+	if err != nil {
+		log.Fatal("Failed to run migrations. \n", err)
+		os.Exit(2)
+	}
+
+	log.Println("Migrations run successfully")
+}
