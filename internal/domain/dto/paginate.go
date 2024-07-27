b760ac35011bbc81df3e6dfea91571dfdfaf860a
KelpGF <kelpgf@gmail.com>
2024-07-27 17:58:52 -0300

    feat: add paginate dto

---
+++ internal/domain/dto/paginate.go
@@ -0,0 +1,24 @@
+package dto
+
+type Paginate struct {
+	Page  int `json:"page"`
+	Limit int `json:"limit"`
+}
+
+func (p *Paginate) GetLimit() int {
+	if p.Limit == 0 {
+		return 10
+	}
+	return p.Limit
+}
+
+func (p *Paginate) GetPage() int {
+	if p.Page == 0 {
+		return 1
+	}
+	return p.Page
+}
+
+func (p *Paginate) Offset() int {
+	return (p.Page - 1) * p.Limit
+}
