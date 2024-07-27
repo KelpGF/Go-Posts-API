54a2cd44855593ca8cdeb63e73d847c4427c4eef
KelpGF <kelpgf@gmail.com>
2024-07-27 18:51:54 -0300

    feat: ensure ListPostsRepository get correct data

---
+++ internal/infrastructure/repositories/list_posts.go
@@ -0,0 +1,33 @@
+package repositories
+
+import (
+	"github.com/KelpGF/Go-Posts-API/internal/domain/dto"
+	"gorm.io/gorm"
+)
+
+type ListPostsRepository struct {
+	db *gorm.DB
+}
+
+func NewListPostsRepository(db *gorm.DB) *ListPostsRepository {
+	return &ListPostsRepository{
+		db: db,
+	}
+}
+
+func (r *ListPostsRepository) List(input *dto.ListPostsInput) *dto.ListPostsOutput {
+	var posts []dto.Post
+	query := r.db.Table("posts")
+
+	if input.AuthorName != "" {
+		query.Where("author_name LIKE ?", "%"+input.AuthorName+"%")
+	}
+
+	query.Order("published_at " + input.PublishedSort).Offset(input.Paginate.Offset()).Limit(input.GetLimit())
+
+	query.Find(&posts)
+
+	return &dto.ListPostsOutput{
+		Posts: posts,
+	}
+}
