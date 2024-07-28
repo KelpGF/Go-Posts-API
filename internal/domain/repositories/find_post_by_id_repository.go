4443826e650bd6909624afadd32bbd9ca5681610
KelpGF <kelpgf@gmail.com>
2024-07-28 10:53:31 -0300

    feat: add FindPostByIdRepository interface

---
+++ internal/domain/repositories/find_post_by_id_repository.go
@@ -0,0 +1,10 @@
+package repositories
+
+import (
+	entities "github.com/KelpGF/Go-Posts-API/internal/domain/entities/id"
+	entityPost "github.com/KelpGF/Go-Posts-API/internal/domain/entities/post"
+)
+
+type FindPostByIdRepository interface {
+	FindById(input *entities.ID) entityPost.Post
+}
